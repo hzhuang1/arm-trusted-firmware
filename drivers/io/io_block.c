9a73361aace99a3e3a42d3769a359824dd6b07ac
Haojian Zhuang <haojian.zhuang@linaro.org>
2016-05-18 14:40:51 +0800

    Revert "block: debug: avoid to copy SRAM"
    This reverts commit 627760ed7c443d688b9f16b0171bd0d20faf927f.

--- drivers/io/io_block.c
+++ drivers/io/io_block.c
@@ -216,7 +216,6 @@ static int block_read(io_entity_t *entity, uintptr_t buffer, size_t length,
 	do {
 		lba = (cur->file_pos + cur->base) / block_size;
 		if (left >= buf->length) {
-#if 0
 			/* Since left is larger, it's impossible to padding. */
 			if (skip) {
 				/*
@@ -241,16 +240,8 @@ static int block_read(io_entity_t *entity, uintptr_t buffer, size_t length,
 				       (void *)(buf->offset + skip),
 				       count - skip);
 			}
-#else
-			count = ops->read(lba, buf->offset, buf->length);
-			assert(count == buf->length);
-			cur->file_pos += count - skip;
-			memcpy((void *)buffer, (void *)(buf->offset + skip),
-			       count - skip);
-#endif
 			left = left - (count - skip);
 		} else {
-#if 0
 			if (skip || padding) {
 				/*
 				 * The beginning address (file_pos) isn't
@@ -274,14 +265,6 @@ static int block_read(io_entity_t *entity, uintptr_t buffer, size_t length,
 				       (void *)(buf->offset + skip),
 				       left);
 			}
-#else
-			count = ops->read(lba, buf->offset, left);
-			assert(count == left);
-			left = left - (skip + padding);
-			cur->file_pos += left;
-			memcpy((void *)buffer, (void *)(buf->offset + skip),
-			       left);
-#endif
 			/* It's already the last block operation */
 			left = 0;
 		}
