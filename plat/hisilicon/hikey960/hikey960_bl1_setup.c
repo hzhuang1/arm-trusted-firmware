9ee4bba5788adde84f4d7547ff49dc7addad8d08
Haojian Zhuang <haojian.zhuang@linaro.org>
2017-04-10 13:48:46 +0800

    hikey960: add regulator setting
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- plat/hisilicon/hikey960/hikey960_bl1_setup.c
+++ plat/hisilicon/hikey960/hikey960_bl1_setup.c
@@ -142,6 +142,11 @@ void bl1_plat_arch_setup(void)
 static void hikey960_clk_init(void)
 {
 	mmio_write_32(CRG_REG_BASE + CRG_CLKDIV3_OFFSET, 0xf0001000);
+
+	/* change ldi0 sel to ppll2 */
+	mmio_write_32(0xfff350b4, 0xf0002000);
+	/* ldi0 20' */
+	mmio_write_32(0xfff350bc, 0xfc004c00);
 }
 
 static void hikey960_pmu_init(void)
@@ -149,6 +154,359 @@ static void hikey960_pmu_init(void)
 	/* clear np_xo_abb_dig_START bit in PMIC_CLK_TOP_CTRL7 register */
 	mmio_clrbits_32(PMU_SSI0_REG_BASE + PMU_SSI0_CLK_TOP_CTRL7_OFFSET,
 			NP_XO_ABB_DIG);
+}
+
+static void hikey960_enable_ppll3(void)
+{
+	/* enable ppll3 */
+	mmio_write_32(0xfff31048, 0x4904305);
+	mmio_write_32(0xfff3104c, 0x2300000);
+	mmio_write_32(0xfff3104c, 0x6300000);
+}
+
+static void bus_idle_clear(unsigned int value)
+{
+	unsigned int pmc_value, pmc_value1, pmc_value2;
+	int timeout = 100;
+
+	pmc_value = value << 16;
+	pmc_value &= ~value;
+	mmio_write_32(0xfff31380, pmc_value);
+
+	for (;;) {
+		pmc_value1 = (unsigned int)mmio_read_32(0xfff31384);
+		pmc_value2 = (unsigned int)mmio_read_32(0xfff31388);
+		if (((pmc_value1 & value) == 0) && ((pmc_value2 & value) == 0))
+			break;
+		udelay(1);
+		timeout--;
+		if (timeout <= 0) {
+			WARN("%s timeout\n", __func__);
+			break;
+		}
+	}
+}
+
+static void set_vivobus_power_up(void)
+{
+	/* clk enable */
+	mmio_write_32(0xfff350f8, 0x00020002);
+	mmio_write_32(0xfff35000, 0x00001000);
+}
+
+static void set_dss_power_up(void)
+{
+	/* set edc0 133MHz = 1600MHz / 12 */
+	mmio_write_32(0xfff350bc, 0x003f000b);
+	/* mtcmos on */
+	mmio_write_32(0xfff35150, 0x00000020);
+	udelay(100);
+	/* DISP CRG */
+	mmio_write_32(0xfff35094, 0x00000010);
+	/* clk enable */
+	mmio_write_32(0xfff350f0, 0x01400140);
+	mmio_write_32(0xfff35000, 0x00002000);
+	mmio_write_32(0xfff35030, 0x0003b000);
+	udelay(1);
+	/* clk disable */
+	mmio_write_32(0xfff35034, 0x0003b000);
+	mmio_write_32(0xfff35004, 0x00002000);
+	mmio_write_32(0xfff350f0, 0x01400000);
+	udelay(1);
+	/* iso disable */
+	mmio_write_32(0xfff35148, 0x00000040);
+	/* unreset */
+	mmio_write_32(0xfff35094, 0x00000006);
+	mmio_write_32(0xfff35088, 0x00000c00);
+	/* clk enable */
+	mmio_write_32(0xfff350f0, 0x01400140);
+	mmio_write_32(0xfff35000, 0x00002000);
+	mmio_write_32(0xfff35030, 0x0003b000);
+	/* bus idle clear */
+	bus_idle_clear(1 << 13);
+	/* set edc0 400MHz for 2K 1600MHz / 4 */
+	mmio_write_32(0xfff350bc, 0x003f0003);
+}
+
+static void set_vcodec_power_up(void)
+{
+	/* clk enable */
+	mmio_write_32(0xfff350f8, 0x00040004);
+	mmio_write_32(0xfff35000, 0x00000060);
+	mmio_write_32(0xfff35020, 0x10000000);
+	/* unreset */
+	mmio_write_32(0xfff35064, 0x00000018);
+	/* bus idle clear */
+	bus_idle_clear(1 << 4);
+}
+
+static void set_vdec_power_up(void)
+{
+	/* mtcmos on */
+	mmio_write_32(0xfff35150, 0x00000004);
+	udelay(100);
+	/* clk enable */
+	mmio_write_32(0xfff350f0, 0x80008000);
+	mmio_write_32(0xfff35020, 0x20080000);
+	mmio_write_32(0xfff35030, 0x00000800);
+	udelay(1);
+	/* clk disable */
+	mmio_write_32(0xfff35034, 0x00000800);
+	mmio_write_32(0xfff35024, 0x20080000);
+	mmio_write_32(0xfff350f0, 0x80000000);
+	udelay(1);
+	/* iso disable */
+	mmio_write_32(0xfff35148, 0x00000004);
+	/* unreset */
+	mmio_write_32(0xfff35088, 0x00000200);
+	/* clk enable */
+	mmio_write_32(0xfff350f0, 0x80008000);
+	mmio_write_32(0xfff35020, 0x20080000);
+	mmio_write_32(0xfff35030, 0x00000800);
+	/* bus idle clear */
+	bus_idle_clear(1 << 10);
+}
+
+static void set_venc_power_up(void)
+{
+	/* set venc ppll3 */
+	mmio_write_32(0xfff350c8, 0x18001000);
+	/* set venc 258MHz, 1290MHz / 5 */
+	mmio_write_32(0xfff350c8, 0x07c00100);
+	/* mtcmos on */
+	mmio_write_32(0xfff35150, 0x00000002);
+	udelay(100);
+	/* clk enable */
+	mmio_write_32(0xfff350f4, 0x00010001);
+	mmio_write_32(0xfff35020, 0x40000100);
+	mmio_write_32(0xfff35030, 0x00000400);
+	udelay(1);
+	/* clk disable */
+	mmio_write_32(0xfff35034, 0x00000400);
+	mmio_write_32(0xfff35024, 0x40000100);
+	mmio_write_32(0xfff350f4, 0x00010000);
+	udelay(1);
+	/* iso disable */
+	/* unreset */
+	mmio_write_32(0xfff35088, 0x00000100);
+	/* clk enable */
+	mmio_write_32(0xfff350f4, 0x00010001);
+	mmio_write_32(0xfff35020, 0x40000100);
+	mmio_write_32(0xfff35030, 0x00000400);
+	/* bus idle clear */
+	bus_idle_clear(1 << 11);
+	/* set venc 645MHz, 1290MHz / 2 */
+	mmio_write_32(0xfff350c8, 0x07c00040);
+}
+
+static void set_isp_power_up(void)
+{
+	/* mtcmos on */
+	mmio_write_32(0xfff35150, 0x00000001);
+	udelay(100);
+	/* clk enable */
+	mmio_write_32(0xfff350f0, 0x70007000);
+	mmio_write_32(0xfff350f8, 0x00100010);
+	mmio_write_32(0xfff35050, 0x01000010);
+	mmio_write_32(0xfff35030, 0x0bf00000);
+	udelay(1);
+	/* clk disable */
+	mmio_write_32(0xfff35054, 0x01000010);
+	mmio_write_32(0xfff35034, 0x0bf00000);
+	mmio_write_32(0xfff350f0, 0x70000000);
+	mmio_write_32(0xfff350f8, 0x00100000);
+	udelay(1);
+	/* iso disable */
+	mmio_write_32(0xfff35148, 0x00000001);
+	/* unreset */
+	mmio_write_32(0xfff35c84, 0x0000002f);
+	/* clk enable */
+	mmio_write_32(0xfff350f0, 0x70007000);
+	mmio_write_32(0xfff350f8, 0x00100010);
+	mmio_write_32(0xfff35050, 0x01000010);
+	mmio_write_32(0xfff35030, 0x0bf00000);
+	/* bus idle clear */
+	bus_idle_clear(1 << 5);
+	/* csi clk enable */
+	mmio_write_32(0xfff35030, 0x00700000);
+}
+
+static void set_ivp_power_up(void)
+{
+	/* set ivp ppll0 */
+	mmio_write_32(0xfff350a8, 0xc0000000);
+	/* set ivp 267MHz, 1600MHz / 6 */
+	mmio_write_32(0xfff350a8, 0x3c001400);
+	/* mtcmos on */
+	mmio_write_32(0xfff35150, 0x00200000);
+	udelay(100);
+	/* IVP CRG unreset */
+	mmio_write_32(0xfff35c04, 0x00000001);
+	/* clk enable */
+	mmio_write_32(0xfff350f8, 0x02000200);
+	mmio_write_32(0xfff35040, 0x000000a8);
+	udelay(1);
+	/* clk disable */
+	mmio_write_32(0xfff35044, 0x000000a8);
+	mmio_write_32(0xfff350f8, 0x02000000);
+	udelay(1);
+	/* iso disable */
+	mmio_write_32(0xfff35148, 0x01000000);
+	/* unreset */
+	mmio_write_32(0xfff35c04, 0x00000002);
+	/* clk enable */
+	mmio_write_32(0xfff350f8, 0x02000200);
+	mmio_write_32(0xfff35040, 0x000000a8);
+	/* bus idle clear */
+	bus_idle_clear(1 << 14);
+	/* set ivp 533MHz, 1600MHz / 3 */
+	mmio_write_32(0xfff350a8, 0x3c000800);
+}
+
+static void set_audio_power_up(void)
+{
+	unsigned int ret;
+	int timeout = 100;
+	/* mtcmos on */
+	mmio_write_32(0xfff0a060, 0x00000001);
+	udelay(100);
+	/* clk enable */
+	mmio_write_32(0xfff350f4, 0x80108010);
+	mmio_write_32(0xfff0a258, 0x00010001);
+	mmio_write_32(0xfff0a160, 0x0c000000);
+	mmio_write_32(0xfff35000, 0x04000000);
+	mmio_write_32(0xfff35050, 0x00000080);
+	mmio_write_32(0xfff0a170, 0x0000000f);
+	udelay(1);
+	/* clk disable */
+	mmio_write_32(0xfff0a174, 0x0000000f);
+	mmio_write_32(0xfff0a164, 0x0c000000);
+	mmio_write_32(0xfff35054, 0x00000080);
+	mmio_write_32(0xfff35004, 0x04000000);
+	mmio_write_32(0xfff0a258, 0x00010000);
+	mmio_write_32(0xfff350f4, 0x80100000);
+	udelay(1);
+	/* iso disable */
+	mmio_write_32(0xfff0a044, 0x00000001);
+	udelay(1);
+	/* unreset */
+	mmio_write_32(0xfff0aa54, 0x00000001);
+	mmio_write_32(0xfff0a204, 0x00000780);
+	/* clk enable */
+	mmio_write_32(0xfff350f4, 0x80108010);
+	mmio_write_32(0xfff0a258, 0x00010001);
+	mmio_write_32(0xfff0a160, 0x0c000000);
+	mmio_write_32(0xfff35000, 0x04000000);
+	mmio_write_32(0xfff35050, 0x00000080);
+	mmio_write_32(0xfff0a170, 0x0000000f);
+	/* bus idle clear */
+	mmio_write_32(0xfff0a31c, 0x00040000);
+	for (;;) {
+		ret = mmio_read_32(0xfff0a378);
+		if (((ret & (1 << 5)) == 0) && ((ret & (1 << 8)) == 0))
+			break;
+		udelay(1);
+		timeout--;
+		if (timeout <= 0) {
+			WARN("%s timeout\n", __func__);
+			break;
+		}
+	}
+	mmio_write_32(0xe804e148, 0x00ff0000);
+}
+
+static void set_pcie_power_up(void)
+{
+	/* mtcmos on */
+	mmio_write_32(0xfff0a060, 0x00000010);
+	udelay(100);
+	/* clk enable */
+	mmio_write_32(0xfff0a268, 0x08000800);
+	mmio_write_32(0xfff0a190, 0x00104000);
+	mmio_write_32(0xfff35420, 0x000003a0);
+	udelay(1);
+	/* clk disable */
+	mmio_write_32(0xfff0a194, 0x00104000);
+	mmio_write_32(0xfff35424, 0x000003a0);
+	mmio_write_32(0xfff0a268, 0x08000000);
+	udelay(1);
+	/* iso disable */
+	mmio_write_32(0xfff0a044, 0x00000030);
+	/* unreset */
+	mmio_write_32(0xfff35088, 0x8c000000);
+	/* clk enable */
+	mmio_write_32(0xfff0a268, 0x08000800);
+	mmio_write_32(0xfff0a190, 0x00104000);
+	mmio_write_32(0xfff35420, 0x000003a0);
+}
+
+static void ispfunc_enable(void)
+{
+	/* enable ispfunc. Otherwise powerup isp_srt causes exception. */
+	mmio_write_32(0xfff35000, 0x00000008);
+	mmio_write_32(0xfff35460, 0xc004ffff);
+	mmio_write_32(0xfff35030, 0x02000000);
+	udelay(10000);
+}
+
+static void isps_control_clock(int flag)
+{
+	unsigned int ret;
+
+	/* flag: 0 -- disable clock, 1 -- enable clock */
+	if (flag) {
+		ret = mmio_read_32(0xe8420364);
+		ret |= 1;
+		mmio_write_32(0xe8420364, ret);
+	} else {
+		ret = mmio_read_32(0xe8420364);
+		ret &= ~1;
+		mmio_write_32(0xe8420364, ret);
+	}
+}
+
+static void set_isp_srt_power_up(void)
+{
+	unsigned int ret;
+
+	ispfunc_enable();
+	/* reset */
+	mmio_write_32(0xe8420374, 0x00000001);
+	mmio_write_32(0xe8420350, 0x00000000);
+	mmio_write_32(0xe8420358, 0x00000000);
+	/* mtcmos on */
+	mmio_write_32(0xfff35150, 0x00400000);
+	udelay(100);
+	/* clk enable */
+	isps_control_clock(1);
+	udelay(1);
+	isps_control_clock(0);
+	udelay(1);
+	/* iso disable */
+	mmio_write_32(0xfff35148, 0x08000000);
+	/* unreset */
+	ret = mmio_read_32(0xe8420374);
+	ret &= ~0x1;
+	mmio_write_32(0xe8420374, ret);
+	/* clk enable */
+	isps_control_clock(1);
+	/* enable clock gating for accessing csi registers */
+	mmio_write_32(0xe8420010, ~0);
+}
+
+static void hikey960_regulator_enable(void)
+{
+	set_vivobus_power_up();
+	hikey960_enable_ppll3();
+	set_dss_power_up();
+	set_vcodec_power_up();
+	set_vdec_power_up();
+	set_venc_power_up();
+	set_isp_power_up();
+	set_ivp_power_up();
+	set_audio_power_up();
+	set_pcie_power_up();
+	set_isp_srt_power_up();
 }
 
 static void hikey960_ufs_reset(void)
@@ -236,6 +594,7 @@ void bl1_platform_setup(void)
 	hikey960_clk_init();
 	hikey960_pmu_init();
 	generic_delay_timer_init();
+	hikey960_regulator_enable();
 	hikey960_tzc_init();
 	hikey960_ufs_init();
 	hikey960_io_setup();
