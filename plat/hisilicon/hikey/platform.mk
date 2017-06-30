bdd8fb77eb952ed5efece1af223100eae13d8e83
Haojian Zhuang <haojian.zhuang@linaro.org>
2017-06-30 16:21:54 +0800

    hikey: enable options to fix errata
    Fix cortex a53 errata issues: #836870, #843419, #855873.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- plat/hisilicon/hikey/platform.mk
+++ plat/hisilicon/hikey/platform.mk
@@ -105,3 +105,8 @@ BL31_SOURCES		+=	drivers/arm/cci/cci.c			\
 				plat/hisilicon/hikey/hisi_pwrc.c	\
 				plat/hisilicon/hikey/hisi_pwrc_sram.S	\
 				${HIKEY_GIC_SOURCES}
+
+# Enable workarounds for selected Cortex-A53 errata.
+ERRATA_A53_836870		:=	1
+ERRATA_A53_843419		:=	1
+ERRATA_A53_855873		:=	1
